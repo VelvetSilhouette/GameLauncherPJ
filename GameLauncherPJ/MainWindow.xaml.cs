9d17717d75c3405e8bea622a7606323ea00083f6
VelvetSilhouette <thanhnamnguyen380@gmail.com>
2023-11-15 15:55:12 +0700

    Fix show password button to be more dynamic

--- GameLauncherPJ/MainWindow.xaml.cs
+++ GameLauncherPJ/MainWindow.xaml.cs
@@ -33,36 +33,27 @@ namespace GameLauncherPJ
             //literally just a close button for the application because Windows Border is ugly af
             this.Close();
         }
-        
         //making a show password button with some trick since wpf passwordbox don't support it
-        private void Passwordtxtbox_GotFocus(object sender, RoutedEventArgs e)
+        private void Passwordtxtbox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            ShowPassword_btn.Visibility = Visibility.Visible;
-        }
-
-        private void Passwordtxtbox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (Passwordtxtbox.Password.ToString() == "") 
-            {
-                ShowPassword_btn.Visibility = Visibility.Hidden;
-            }         
+            ShowPassword_btn.Visibility = Passwordtxtbox.Password.Length > 0 || Passwordshowtxtbox.Text.Length > 0 ? Visibility.Visible : Visibility.Hidden;
         }
         //show/hide password when clicking
         private void ShowPassword_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (Passwordshowtxtbox.Visibility == Visibility.Hidden) 
+            //Show password button :>>>>>
+            if(Passwordshowtxtbox.Visibility == Visibility.Visible) 
             {
-                 Passwordshowtxtbox.Text = Passwordtxtbox.Password.ToString();
-                Passwordtxtbox.Visibility = Visibility.Hidden;
-                Passwordshowtxtbox.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                Passwordtxtbox.Password = Passwordshowtxtbox.Text;
-                Passwordtxtbox.Visibility = Visibility.Visible;
                 Passwordshowtxtbox.Visibility = Visibility.Hidden;
+                Passwordtxtbox.Visibility = Visibility.Visible;
+                Passwordtxtbox.Password = Passwordshowtxtbox.Text;
             }
-           
+            else 
+            {
+                Passwordtxtbox.Visibility=Visibility.Hidden;
+                Passwordshowtxtbox.Visibility = Visibility.Visible;
+                Passwordshowtxtbox.Text = Passwordtxtbox.Password.ToString();
+            }
         }
         //limit password length to 16 characters
         private void Passwordtxtbox_KeyDown(object sender, KeyEventArgs e)
@@ -77,6 +68,7 @@ namespace GameLauncherPJ
             { e.Handled = true;}    
         }
         //Login button, for now use a test account to test login to MainMenu
+        //id: admin | password:123456
         private void Login_btn_Click(object sender, RoutedEventArgs e)
         {
             if(Usernametxtbox.Text == testaccountid)
@@ -87,8 +79,10 @@ namespace GameLauncherPJ
                     mainMenu.Show();
                     this.Close();
                 }
+                else 
+                {MessageBox.Show("Please Enter the right account, retard","Retard Alert")}
             }
-
+        
         }
     }
 }
